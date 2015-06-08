d1b1104e4bb39d8a0a66d48683baf52c142cdf00
Alex Gao <alex@gatherhealth.com>
2015-06-08 22:50:58 +0800

    add xmlrunner to generate test reprot

--- Nurse_A/Script/Suites/PR_R_U.py
+++ Nurse_A/Script/Suites/PR_R_U.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import unittest
+import unittest, xmlrunner
 
 Loader = unittest.defaultTestLoader
 Loader.testMethodPrefix = 'test_urgent'
 suites = Loader.discover('../PR_Regression', pattern='Test_*.py')
-Runner = unittest.TextTestRunner(verbosity=2)
+Runner = xmlrunner.XMLTestRunner(output='../reports')
 result = Runner.run(suites)
