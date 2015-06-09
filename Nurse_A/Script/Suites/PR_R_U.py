177facb77f0479f3933906dcdc52959d61adf0a5
Alex Gao <alex@gatherhealth.com>
2015-06-09 11:26:44 +0800

    update PR_R_U.py

--- Nurse_A/Script/Suites/PR_R_U.py
+++ Nurse_A/Script/Suites/PR_R_U.py
@@ -4,6 +4,8 @@ import unittest, xmlrunner
 
 Loader = unittest.defaultTestLoader
 Loader.testMethodPrefix = 'test_urgent'
-suites = Loader.discover('../PR_Regression', pattern='Test_*.py')
-Runner = xmlrunner.XMLTestRunner(output='../reports')
+suites = Loader.discover('../PR_Regression', pattern='Test_account*.py')
+# suites = Loader.loadTestsFromModule('../PR_Regression/Test_account_settings')
+Runner = xmlrunner.XMLTestRunner(output='../reports', verbosity=2)
+# Runner = unittest.TextTestRunner(verbosity=2)
 result = Runner.run(suites)
