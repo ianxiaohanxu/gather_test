e774b386dae750e7ce3bbc4f32cc83a6a0ef9029
Alex Gao <alex@gatherhealth.com>
2015-06-09 15:52:46 +0800

    update jenkins script

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -19,10 +19,13 @@ popd
 
 # Run test on virtual screen
 # export PATH=$PATH:/usr/local/bin
-# export DISPLAY=:0
+Xvfb :99 -ac &
+export DISPLAY=:99
 
 # Run test scripts
 pushd Nurse_A/Script/Suites
 python PR_R_U.py
 # python Nurse_A/Script/PR_Regression/Test_account_settings.py
 popd
+
+killall Xvfb
