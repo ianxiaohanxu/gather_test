1af0ac83eefc708db720fc1484e47607b1dbcf12
Alex Gao <alex@gatherhealth.com>
2015-09-16 17:40:19 +0800

    small tweak for feed cases

--- Nurse_A/Script/PR_Regression/Test_feed.py
+++ Nurse_A/Script/PR_Regression/Test_feed.py
@@ -22,10 +22,10 @@ class Feed(Case):
     def setUp(self):
         self.pr = WEB(server = data.SERVER)
         self.pr.driver.maximize_window()
-        self.demo = setup.demo_data
+        self.demo = self.pr.generate_test_demo()
         
     def tearDown(self):
-        # self.pr.delete_test_demo(self.demo[1])
+        self.pr.delete_test_demo(self.demo[1])
         self.pr.teardown()
         
     def test_urgent_dismiss_event_by_chat(self):
@@ -34,7 +34,6 @@ class Feed(Case):
         This test is for '108002 Complete the event by chat'
         '''
         MES = 'hello, world!'
-        # self.demo = self.pr.generate_test_demo()
         self.pr.login(data.DOCTOR, self.demo[0])
         self.pr.click(data.PR_FEED_FIRST_WARNING)
         self.pr.verify(data.PR_PATIENT_RECORD_CHAT)
@@ -54,7 +53,6 @@ class Feed(Case):
         108003
         This test is for '108003 Complete the event by update MED goals'
         '''
-        # self.demo = self.pr.generate_test_demo()
         self.pr.login(data.DOCTOR, self.demo[0])
         self.pr.click(data.PR_FEED_FIRST_WARNING)
         self.pr.verify(data.PR_PATIENT_RECORD_PRESCRIPTION)
@@ -76,7 +74,6 @@ class Feed(Case):
         108004
         This test is for '108004 Complete the event by update BG goals'
         '''
-        # self.demo = self.pr.generate_test_demo()
         self.pr.login(data.DOCTOR, self.demo[0])
         self.pr.click(data.PR_FEED_FIRST_WARNING)
         self.pr.verify(data.PR_PATIENT_RECORD_SMBG)
@@ -95,7 +92,6 @@ class Feed(Case):
         108005
         This test is for '108005 Delete event in feed'
         '''
-        # self.demo = self.pr.generate_test_demo()
         self.pr.login(data.DOCTOR, self.demo[0])
         self.pr.click(data.PR_FEED_CLOSE)
         self.pr.refresh()
@@ -107,7 +103,6 @@ class Feed(Case):
         This test is for '108006 Forward event in feed'
         '''
         notes = 'It is up to you.'
-        # self.demo = self.pr.generate_test_demo()
         self.pr.login(data.DOCTOR, self.demo[0])
         self.pr.click(data.PR_FEED_FORWARD)
         self.pr.verify(data.PR_FEED_FORWARD_TO)
@@ -124,7 +119,6 @@ class Feed(Case):
         106013
         This test is for '106013 Delete messages event in Feed'
         '''
-        # self.demo = self.pr.generate_test_demo()
         self.pr.login(data.NURSE, self.demo[0])
         self.pr.clear_message_event()
         self.pr.logout()
@@ -136,7 +130,6 @@ class Feed(Case):
         106014
         This test is for '106014 Complete messages event by chat'
         '''
-        # self.demo = self.pr.generate_test_demo()
         self.pr.login(data.NURSE, self.demo[0])
         MES = 'hello, world!'
         NAME = 'Uttam'
