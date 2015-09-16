fe466fc6259d1d863bb98ecb8c4daa9907a683ca
Alex Gao <alex@gatherhealth.com>
2015-09-16 16:33:11 +0800

    small tweak

--- Nurse_A/Script/Suites/PR_R_U.py
+++ Nurse_A/Script/Suites/PR_R_U.py
@@ -7,7 +7,7 @@ from Nurse_A.Settings import setup
 
 Loader = unittest.defaultTestLoader
 Loader.testMethodPrefix = 'test'
-suites = Loader.discover('../PR_Regression', pattern='Test_account*.py')
+suites = Loader.discover('../PR_Regression', pattern='Test_*.py')
 Runner = xmlrunner.XMLTestRunner(output='../reports', verbosity=2)
 result = Runner.run(suites)
 setup.delete_test_demo(setup.demo_data[1])
