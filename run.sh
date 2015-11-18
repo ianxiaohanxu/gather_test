58f6462b26ad4c790cf6a92dd8807d258c0266a1
Alex Gao <alex@gatherhealth.com>
2015-11-18 14:26:33 +0800

    update requirements and add new run script

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Activate virtualenv
+if
+source ./venv/bin/activate
+then
+echo 'Activate virtualenv'
+else
+exit 1
+fi
+
+# Intall dependence modules
+pip install --upgrade -r requirements/test.txt
+
+# Install loacal package
+pushd Nurse_A
+python setup.py sdist
+python setup.py install
+
+popd
+
+# Run test scripts
+pushd Nurse_A/Script/Suites
+rm -rf ../reports/*
+python PR_R_U.py
+
